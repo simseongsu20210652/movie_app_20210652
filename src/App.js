cfc412a6d4cda86a74e8977e108321fa155fde12
NOS-Nitrogen <maskmax@naver.com>
2022-05-12 10:36:07 +0900

    07장

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import PropTypes from "prop-types";
+import React from "react";
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-    <h2>I like {name} </h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async() => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+      this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } =this.state;
+    return <section className="container">
+      {isLoading ? (
+        <div className="loader">
+        <span className="loader__text">'Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+      {movies.map((movie) => (
+    
+      <Movie 
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+      />
+      
+      ))}
     </div>
-  );
-}
-
-const foodILike = [
-  {
-    name: 'Kimchis',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.1store.io%2FofficialProductImage%2F1587198787425-0712162682417814.png&f=1&nofb=1',
-  },
-  {
-    name: 'Samgyeopsal',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmp-seoul-image-production-s3.mangoplate.com%2Fmango_pick%2Fuker6u9xhkr1m8.jpg&f=1&nofb=1',
-  },
-  {
-    name: 'Bibimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Frgo4.com%2Ffiles%2Fattach%2Fimages%2F2651950%2F417%2F385%2F011%2F603b6c64a737ea2f484f07b98b5b956b.jpg&f=1&nofb=1',
-  },
-  {
-    name: 'Doncasu',
-    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcfile6.uf.tistory.com%2Fimage%2F2116093F58367AE114E6CB&f=1&nofb=1',
-  },
-  {
-    name: 'Kimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Frecipe1.ezmember.co.kr%2Fcache%2Frecipe%2F2019%2F02%2F04%2F5b51bc5c37041914f8f65bf19a8883df1.jpg&f=1&nofb=1',
-  },
-];
-
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish => 
-    <Food key={dish.name} name={dish.name} picture={dish.image} rating={dish.rating} />
     )}
-    </div>
-  );
+    </section>;
+    
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie( { title, year, summary, poster, genres } ) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+        <div className="movie__date">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+                {genres.map((genre, index) => {
+                    return <li key={index}className="movie__genre">{genre}</li>;
+                })
+                }
+            </ul>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+        </div>
+    )
+}
+
+Movie.propType = { 
+    
+    year: PropTypes.number.isReaqured,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+
+};
+
+export default Movie;
